705204652d44ad36dc5b27238756e2ad8b5eaf87
py3-dev <[200~yash.varshney@careers360.in>
2023-07-19 11:21:27 +0530

    templates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {'author':'CoreyMS',
+     'title':'Blog Post 1',
+     'content':'First post content',
+     'date_posted':'July 18, 2023'},
+     {'author':'Yash Varshney',
+     'title':'Blog Post 2',
+     'content':'second post content',
+     'date_posted':'January 18, 2021'},
+]
+
 def home(request):
-    return HttpResponse(
-        '<h1>Blog Home</h1>'
-    )
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse(
-        '<h1>Blog About</h1>'
-    )
+    return render(request,'blog/about.html',{'title':'Glad to have you!'})
